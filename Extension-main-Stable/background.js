b53db4d2bf214be23463d36891bf2821fb2282e4
Whitzzscott <samytplayz197@gmail.com>
2024-11-22 22:36:06 +0800

    Update background.js

--- Extension-main-Stable/background.js
+++ Extension-main-Stable/background.js
@@ -1,13 +1,56 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ autoLoad: true, sakuraEnhance: true }, () => {
-        console.log("Default settings have been set.");
+    chrome.storage.sync.set({ 
+        autoLoad: true,
+        sakuraEnhance: true,
+        showPTag: true,
+        autoTrigger: true,
+        customFont: '',
+        fontSize: '16px',
+        theme: 'dark'
+    }, () => {
+        console.log("Extension installed: Default settings initialized");
     });
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "getSettings") {
-        chrome.storage.sync.get(["autoLoad", "sakuraEnhance"], (data) => {
+        chrome.storage.sync.get([
+            "autoLoad", 
+            "sakuraEnhance", 
+            "showPTag",
+            "autoTrigger",
+            "customFont",
+            "fontSize",
+            "theme"
+        ], (data) => {
+            data.showPTag = true;
+            data.autoTrigger = true;
             sendResponse(data);
+        });
+        return true;
+    } 
+    
+    if (request.action === "updateShowPTag") {
+        chrome.storage.sync.set({ 
+            showPTag: true,
+            autoTrigger: true 
+        }, () => {
+            sendResponse({ success: true });
+        });
+        return true;
+    }
+
+    if (request.action === "resetSettings") {
+        chrome.storage.sync.set({
+            autoLoad: true,
+            sakuraEnhance: true, 
+            showPTag: true,
+            autoTrigger: true,
+            customFont: '',
+            fontSize: '16px',
+            theme: 'dark'
+        }, () => {
+            sendResponse({ success: true });
         });
         return true;
     }
