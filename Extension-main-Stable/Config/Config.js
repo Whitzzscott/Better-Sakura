b75389caa846f1ab27c9828c93e32ca24a025f6e
Whitzzscott <samytplayz197@gmail.com>
2025-03-07 18:18:21 +0800

    Delete Extension-main-Stable directory

--- Extension-main-Stable/Config/Config.js
+++
@@ -1,1 +0,0 @@
-export const BASEURL = 'https://bettersakuraserver-production.up.railway.app';
--- Extension-main-Stable/Scripts/AnimateInject.js
+++
@@ -1,8 +0,0 @@
-const injectAnimateCSS = () => {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css';
-    document.head.appendChild(link);
-};
-
-injectAnimateCSS();
